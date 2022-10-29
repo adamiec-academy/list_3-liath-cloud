41a8ad082a30b79e1b3aab058bb96e7c4007eec4
liath-cloud <115594751+liath-cloud@users.noreply.github.com>
2022-10-30 00:04:48 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,22 @@
 def fibonacci_sequence(n):
-    return []
+
+    amount = 0
+    result = []
+
+    while amount != n:
+
+        if n == 1:
+            result.append(0)
+            amount += 1
+
+        elif n == 2 or amount < 2:
+            result.append(0)
+            result.append(1)
+            amount += 2
+
+        else:
+            next = result[-1] + result[-2]
+            result.append(next)
+            amount += 1
+
+    print(result)
