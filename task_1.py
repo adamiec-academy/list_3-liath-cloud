9a75834e3cb2300d0b4a0e0c842d2f8d94ace0ca
liath-cloud <115594751+liath-cloud@users.noreply.github.com>
2022-10-30 00:25:01 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,19 @@
 def get_perfect_numbers(n):
-    return []
+    result = []
+    amount = 0
+    no = 6
+
+    while amount != n:
+        dzielniki = [1]
+
+        for i in range(2, no):
+            if no % i == 0 and i not in dzielniki:
+                dzielniki.append(i)
+
+
+        if sum(dzielniki) == no:
+            result.append(no)
+            amount += 1
+
+        no += 1
+    print(result)
