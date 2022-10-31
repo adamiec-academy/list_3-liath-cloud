9d20136d7c66a6afa2c022f4a4077a2d2640ed21
liath-cloud <115594751+liath-cloud@users.noreply.github.com>
2022-10-31 21:48:39 +0000

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,1 +1,18 @@
-# Remove this comment to confirm that this task is done
+from random import randint
+from turtle import *
+
+speed('fast')
+
+no_steps = 15
+y_axis = [0, 15, 35, 50, 75, 105, 100, 70, 40, 35, 20, 5, 0]
+
+for i in range(len(y_axis)):
+    goto( i * no_steps, y_axis[i] + randint(-20, 20))
+
+forward(20)
+
+for i in range(len(y_axis), len(y_axis) * 2):
+    goto( i * no_steps, y_axis[i - len(y_axis)] + randint(-20, 20))
+
+
+exitonclick()
